901679a8720476f4da62b3d5cf0d3a3a44b16795
vanlinh2002 <vanlinhlinhcang@gmail.com>
2022-10-15 17:06:50 +0700

    Thay doi phep cong

--- Caclulator/Form1.Designer.cs
+++ Caclulator/Form1.Designer.cs
@@ -31,9 +31,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -67,26 +67,26 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Kết quả";
             // 
-            // textBox1
+            // txtA
             // 
-            this.textBox1.Location = new System.Drawing.Point(188, 110);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(231, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtA.Location = new System.Drawing.Point(188, 110);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(231, 20);
+            this.txtA.TabIndex = 1;
             // 
-            // textBox2
+            // txtB
             // 
-            this.textBox2.Location = new System.Drawing.Point(188, 151);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(231, 20);
-            this.textBox2.TabIndex = 1;
+            this.txtB.Location = new System.Drawing.Point(188, 151);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(231, 20);
+            this.txtB.TabIndex = 1;
             // 
-            // textBox3
+            // txtKQ
             // 
-            this.textBox3.Location = new System.Drawing.Point(188, 191);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(231, 20);
-            this.textBox3.TabIndex = 1;
+            this.txtKQ.Location = new System.Drawing.Point(188, 191);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(231, 20);
+            this.txtKQ.TabIndex = 1;
             // 
             // button1
             // 
@@ -97,6 +97,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Cộng";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -104,9 +105,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(666, 425);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -122,9 +123,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtKQ;
         private System.Windows.Forms.Button button1;
     }
 }
--- Caclulator/Form1.cs
+++ Caclulator/Form1.cs
@@ -17,6 +17,12 @@ namespace Caclulator
             InitializeComponent();
         }
 
-     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKQ.Text = c.ToString();
+        }
     }
 }
